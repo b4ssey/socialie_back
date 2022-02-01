260cca3c800cb6fb97d1adf5c38998a1e6f3d1f9
Ekemini <42721358+b4ssey@users.noreply.github.com>
2022-02-01 15:55:16 +0000

    1st commit on socialie backend app

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const logger = require("./middleware/logger");
+
+require("./startup/logging")();
+require("./startup/routes")(app);
+require("./startup/db")();
+require("./startup/config")();
+require("./startup/validation")();
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => logger.info(`Listening on port ${port}`));
+
+module.exports = server;
