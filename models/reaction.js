67670e37c67875eee0cd7856f2e4c974b4ac9856
Ekemini <42721358+b4ssey@users.noreply.github.com>
2022-06-07 23:38:48 +0100

    user can like post

--- models/reaction.js
+++ models/reaction.js
@@ -8,9 +8,25 @@ const Reaction = mongoose.model(
       type: String,
       required: true,
     },
-    postId: {
+    // captionType: { type: String, enum: ["post", "comment"], required: true },
+    captionId: {
       type: String,
       required: true,
     },
+    date: { type: Date, required: true, default: Date.now },
   })
 );
+
+function validateReaction(wnrd, cptnd) {
+  const schema = Joi.object({
+    ownerId: Joi.objectId().required(),
+    captionId: Joi.objectId().required(),
+  });
+  return schema.validate({
+    ownerId: wnrd,
+    captionId: cptnd,
+  });
+}
+
+exports.Reaction = Reaction;
+exports.validate = validateReaction;
--- routes/reactions.js
+++ routes/reactions.js
@@ -1,10 +1,46 @@
 const express = require("express");
 const auth = require("../middleware/auth");
+const { Post } = require("../models/post");
+const { validate, Reaction } = require("../models/reaction");
+const { User } = require("../models/user");
 const router = express.Router();
 
-// router.post("/like", auth, (req, res) => {
-//   let { ownerId, commentId } = req.body;
-//   const {error} =
-// });
+router.post("/like", auth, async (req, res) => {
+  let { ownerId, postId } = req.body;
+  const { error } = validate(ownerId, postId);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const user = await User.findById(ownerId);
+  if (!user) return res.status(400).send("Invalid user.");
+
+  const post = await Post.findById(postId);
+  if (!post) return res.status(400).send("Invalid post.");
+
+  let reaction = new Reaction({
+    ownerId,
+    captionId: postId,
+  });
+
+  const session = Post.startSession();
+
+  try {
+    await reaction.save();
+    (await session).withTransaction(async () => {
+      await Post.updateOne(
+        { _id: postId },
+        {
+          $push: {
+            like: { userId: ownerId, date: reaction.date },
+          },
+        }
+      );
+    });
+    (await session).commitTransaction();
+  } catch (error) {
+    (await session).abortTransaction();
+  }
+
+  res.send(reaction);
+});
 
 module.exports = router;
